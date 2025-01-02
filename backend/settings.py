8ab84227da11a07a9093a91275f476b977769bfd
Sajin Prasad <sajinprasad52@gmail.com>
2025-01-02 19:18:47 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-%+@4d=v34b8k6)fz9$2f#d&9^##yc25@ys#aavu1zuq!46np#$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["blog-app-pq7u-9w0085erk-sajin-prsads-projects.vercel.app", "backend-ekz1.onrender.com"]
+ALLOWED_HOSTS = ["backend-ekz1.onrender.com"]
 
 
 # Application definition
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "https://blog-app-pq7u-9w0085erk-sajin-prsads-projects.vercel.app"
+    "https://blog-app-pq7u.vercel.app/"
 ]
 
 
